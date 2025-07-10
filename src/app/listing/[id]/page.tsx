a1c5c8d31aad53ad52c46f73363757be1fcf009f
emre <emreulup4002@gmail.com>
2025-07-10 09:40:16 +0300

    the new one

--- src/app/listing/[id]/page.tsx
+++ src/app/listing/[id]/page.tsx
@@ -5,7 +5,6 @@ import Link from 'next/link'
 import { ArrowLeft } from 'lucide-react'
 import { Button } from '@/components/ui/button'
 import Image from 'next/image'
-import { Database } from '@/lib/database.types'
 
 
 
